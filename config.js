5c8fd4b2fa7adf9c1bc6d19f5043b0589afd2c55
Kshitij Agarwal <agarwal.kshitij01@gmail.com>
2023-12-10 17:51:07 +0530

    update

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ module.exports = {
     googleAuth: {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:5000/auth/google/callback",
+        callbackURL:
+            "https://cute-gold-goose-vest.cyclic.app/auth/google/callback",
     },
     jwtSecret: process.env.JWT_SECRET,
 };
